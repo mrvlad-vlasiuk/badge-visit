e892a2aa729408263cf6ed4cd43db0fc8db7ccc5
mrvlad-vlasiuk <mr.vladvlasiuk@gmail.com>
2018-12-02 21:03:51 -0700

    create Spring Boot project
    Core: DevTools, Lombok;
    Web: Web;
    SQL: Jpa, MySQL;
    Integration: RabbitMQ;
    
    add minimal properties for green ApplicationTest
    add local variable
    CLEARDB_DATABASE_URL=jdbc:mysql://root:Pass4321@localhost/visitor

---
+++ src/main/java/com/galvanize/badgevisit/BadgeVisitApplication.java
@@ -0,0 +1,12 @@
+package com.galvanize.badgevisit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadgeVisitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BadgeVisitApplication.class, args);
+    }
+}
